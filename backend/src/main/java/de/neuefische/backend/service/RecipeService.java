7c8748199a3f0c08ec16cf666392f120a79c7088
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-10-06 11:09:43 +0200

    insert TestArray/Data

--- backend/src/main/java/de/neuefische/backend/service/RecipeService.java
+++ backend/src/main/java/de/neuefische/backend/service/RecipeService.java
@@ -3,9 +3,16 @@ package de.neuefische.backend.service;
 import de.neuefische.backend.model.Recipe;
 import de.neuefische.backend.repository.RecipeRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class RecipeService {
+
+
+    public List<Recipe> testList = new ArrayList<>(
+            List.of(new Recipe("1", "Nudeln", "5 minuten kochen")
+    ));
 
     private final RecipeRepo recipeRepo;
     public RecipeService(RecipeRepo recipeRepo) {
@@ -13,7 +20,8 @@ public class RecipeService {
     }
 
     public List <Recipe> getAll (){
-        return recipeRepo.findAll();
+        //return recipeRepo.return();
+        return testList;
     }
 
 

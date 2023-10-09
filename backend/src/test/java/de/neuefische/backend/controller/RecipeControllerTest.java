07eeefe3d8f49bfdfdccf012f28bf40e71424c85
JoergWolff <neuerfisch@wolffclan.de>
2023-10-09 12:35:33 +0200

    webserver-test4

--- backend/src/test/java/de/neuefische/backend/controller/RecipeControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/RecipeControllerTest.java
@@ -1,19 +1,14 @@
 package de.neuefische.backend.controller;
 
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.BeforeAll;
+import de.neuefische.backend.model.Recipe;
+import de.neuefische.backend.repository.RecipeRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.io.IOException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,43 +17,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class RecipeControllerTest {
 
-
+    @Autowired
+    RecipeRepo recipeRepo;
     @Autowired
     MockMvc mockMvc;
-
-    private static MockWebServer mockWebServer;
-
-    @BeforeAll
-    static void setup() throws IOException {
-        mockWebServer = new MockWebServer();
-        mockWebServer.start();
-    }
-
-    @DynamicPropertySource
-    static void setUrlDynamically(DynamicPropertyRegistry registry) {
-        registry.add("http://localhost:8080/api/recipes", () -> mockWebServer.url("/").toString());
-    }
 
     @Test
     @DirtiesContext
     void getAllRecipes() throws Exception {
+        recipeRepo.save(new Recipe("1", "Nudeln", "5 minuten kochen"));
 
-        mockWebServer.enqueue(new MockResponse()
-                .setBody(
-                """
-                       [
-                     {
-                        "id": "1",
-                        "title": "Nudeln",
-                        "description": "5 minuten kochen"
-                     }
-                    ]
-                        """
-                )
-                .addHeader("Content-Type", "application/json")
-        );
-
-    mockMvc.perform(MockMvcRequestBuilders.get("/api/recipes"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/recipes"))
             .andExpect(status().isOk())
             .andExpect(content().json("""
                     [

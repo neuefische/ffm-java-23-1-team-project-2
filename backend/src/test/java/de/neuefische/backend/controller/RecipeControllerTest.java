4bc5e486b920a8d4e243869b52c4e5379ea2956b
kristinabusch <Kristina.Busch@sales-forum.de>
2023-10-09 10:54:33 +0200

    add testMock

--- backend/src/test/java/de/neuefische/backend/controller/RecipeControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/RecipeControllerTest.java
@@ -3,13 +3,19 @@ package de.neuefische.backend.controller;
 
 import de.neuefische.backend.model.Recipe;
 import de.neuefische.backend.repository.RecipeRepo;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,8 +24,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class RecipeControllerTest {
 
+
     @Autowired
     MockMvc mockMvc;
+
+    private static MockWebServer mockWebServer;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @DynamicPropertySource
+    static void setUrlDynamically(DynamicPropertyRegistry registry) {
+        registry.add("/api/recipes", () -> mockWebServer.url("/").toString());
+    }
 
     @Autowired
     RecipeRepo recipeRepo;
